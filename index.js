e7c3e90d46c9402d1706c188525349533b43a0a0
MuchinaWamburia <lucymuchina20@gmail.com>
2022-12-16 14:13:25 +0300

    Append

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
 // GET request using fetch()
-fetch('http://localhost:3000/coins' )
+fetch('http://localhost:3000/exchanges' )
 // Converting received data to JSON
  .then((response) => response.json ())
  .then((json) => {
 
 // variable to store html headers
   let li = `<tr>
-  <th>ID</th>
-  <th> Name  </th>
-  <th>Symbol</th>
+  <th> COIN NAME  </th>
+  <th>SYMBOL</th>
   </tr>`; 
 
-  json.forEach((coin) => {
+  json.forEach((exchange) => {
     li+=`<tr>
-    <td>${coin.id}</td>
-    <td> ${coin.Name }</td>
-    <td>${coin.symbol}</td>
-    <td> <div class="likes-section">
-    <span class="likes"> 0 likes</span>
-    <button class="like-button">♡</button>
+      <td> ${exchange.Name }</td>
+    <td><img src= "${exchange.image}"></td>
+    <td> <!-- Use an element to toggle between a like/dislike icon -->
+    <i onclick="myFunction(this)" class="fa fa-thumbs-up"></i>
     </div> </td>
     </tr>`; 
   
   })
 // Setting innerHTML as tab variable
-document.getElementById("coins").innerHTML = li;
+document.getElementById("exchanges").innerHTML = li;
  });
 
-function coin (){
+ function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
 
-  const likeCounter=document.querySelector ('. likes')
-  likeCounter.innerText= `${json.likes} likes`
-
-  const likeButton =document.querySelector ('.like-button')
-  likeButton.addEventListener ('click' , function (event){
-    likeCounter.innerText =incrementLikes (json)
-  })
+  
 }
 
-function incrementLikes (coin) {
-let likes =0
-fetch (`http://localhost:3000/coins ${coin.id}`)
-.then((response) => response.json ())
-.then((json) => {
-  likes=json.likes
+// function exchange (){
 
-})
-let newLikes=likes +1
+//   const likeCounter=document.querySelector ('. likes')
+//   likeCounter.innerText= `${json.likes} likes`
 
-fetch (`http://localhost:3000/coins/1`,{
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify( {
-      "likes": newLikes
-    })
-  })
+//   const likeButton =document.querySelector ('.like-button')
+//   likeButton.addEventListener ('click' , function (event){
+//     likeCounter.innerText =incrementLikes (json)
+//   })
+// }
+
+// function incrementLikes (exchange) {
+// let likes =0
+// fetch (`http://localhost:3000/exchanges ${exchange.id}`)
+// .then((response) => response.json ())
+// .then((json) => {
+//   likes=json.likes
+
+// })
+// let newLikes=likes +1
+
+// fetch (`http://localhost:3000/exchanges/1`,{
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify( {
+//       "likes": newLikes
+//     })
+//   })
  
-let likesText =`${newLikes} likes`
-return likesText
-}
-
+// let likesText =`${newLikes} likes`
+// return likesText
+// }
