7aaff593c29dbf03ca91a299244bee16caa16223
MuchinaWamburia <lucymuchina20@gmail.com>
2022-12-19 00:43:34 +0300

    updated html

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// document.addEventListener(domcon// GET request using fetch()
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+function fetchData(){
 fetch('http://localhost:3000/exchanges' )
 // Converting received data to JSON
  .then((response) => response.json ())
@@ -6,97 +9,64 @@ fetch('http://localhost:3000/exchanges' )
 
 // variable to store html headers
   let li = `<tr>
+  <th>Coin Rank</th>
   <th> COIN NAME  </th>
   <th>SYMBOL</th>
   <th>Year</th>
   <th>Country</th>
-  <th>Coin Rank</th>
   <th>url</th>
+  <th>trade_volume_24h_btc</th>
   </tr>`; 
 
   json.forEach((coins) => {
     li+=`<tr>
-      <td > ${coins.Name }</td>
-      <td><img src= "${coins.image}"></td>
-      <td>${coins.year_established}</td>
+    <td>${coins.trust_score_rank}</td>
+    <td >   ${coins.Name }
+    <p><button type="button" class="btn">
+    <span class="glyphicon glyphicon-thumbs-up"></span> Like
+    </button></p></td>
+    <td><img src= "${coins.image}" title="${coins.description}"></td>
+    <td>${coins.year_established}</td>
     <td>${coins.country}</td>
-      <td>${coins.trust_score_rank}</td>
-      <td><a href="${coins.url}" target="self">${coins.url}</a></td>
-    <td> <i onclick="myFunction(this)" class="fa fa-thumbs-up"></i></td>
-   </tr>`; 
+    <td><a href="${coins.url}" target="blank">${coins.url}</a></td>
+    <td>${coins.trade_volume_24h_btc}</td>
+    </tr>`; 
   })
 // Setting innerHTML as tab variable
 document.getElementById("exchanges").innerHTML = li;
- });
-
- function myFunction(x) {
-  x.classList.toggle("fa-thumbs-down");
- 
+ })
 }
+
 
 fetch("http://localhost:3000/trending")
 // Converting received data to JSON
  .then((resource) => resource.json ())
  .then((data) => {
-
-// variable to store html headers
-  let topCoin =`<tr>
+ // variable to store html headers
+  let topCoin=
+  `<tr>
   <th> COIN NAME  </th>
   <th>SYMBOL</th>
-  
-</tr>`
+  <th>Coin Rank</th>
+  <th>Country</th>
+  <th>url</th>
+  <th>trade_volume_24h_btc</th>
+  <th>trade_volume_24h_btc_normalized</th>
+  </tr>`
 
-
-
-  data.forEach((topC) => {
-    topCoin+=`<tr>
-      <td > ${topC.Name }        <ul>
-      <li class="like">Like! <span class="like-glyph">&#x2661;</span></li>
-    </ul></td>
-      <td><img src= "${topC.image}"></td>
-       </tr>`; 
+  data.forEach((myCoin) => {
+    topCoin += 
+   `<tr>
+      <td > ${myCoin.Name } </td>
+      <td><img src= "${myCoin.image}"></td> 
+      <td>${myCoin.trust_score_rank}</td>
+      <td>${myCoin.country}</td>
+      <td><a href="${myCoin.url}" target="blank">${myCoin.url}</a></td>
+      <td>${myCoin.trade_volume_24h_btc}</td>
+      <td>${myCoin.trade_volume_24h_btc_normalized}</td>
+      </tr>`; 
   })
 // Setting innerHTML as tab variable
-document.getElementById("trending").innerHTML =topCoin;
+document.getElementById("trending").innerHTML =topCoin
+ })
 
-
- });
-
-//  function myFunction(x) {
-//   x.classList.toggle("fa-thumbs-down");
-//  }
-
-// function coins (){
-
-//   const likeCounter=document.querySelector ('. likes')
-//   likeCounter.innerText= `${json.likes} likes`
-
-//   const likeButton =document.querySelector ('.like-button')
-//   likeButton.addEventListener ('click' , function (event){
-//     likeCounter.innerText =incrementLikes (json)
-//   })
-// }
-
-// function incrementLikes (coins) {
-// let likes =0
-// fetch (`http://localhost:3000/exchanges ${coins.id}`)
-// .then((response) => response.json ())
-// .then((json) => {
-//   likes=json.likes
-
-// })
-// let newLikes=likes +1
-
-// fetch (`http://localhost:3000/exchanges/1`,{
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify( {
-//       "likes": newLikes
-//     })
-//   })
- 
-// let likesText =`${newLikes} likes`
-// return likesText
-// }
